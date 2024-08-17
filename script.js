5d4593739e33b49f2321859dd451ba6c6c990fb0
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2024-08-17 12:42:37 +0500

    Class Coffee

---
+++ script.js
@@ -0,0 +1,8 @@
+const Bars = document.querySelector('.bars')
+const Navbar = document.querySelector('.navbar')
+
+
+const Toggle = () =>{
+    Bars.classList.toggle('close')
+    Navbar.classList.toggle('active')
+}
